71329318260141120697ace7886e2f36b97808ab
NePalDevelop <96785307+NePalDevelop@users.noreply.github.com>
2022-01-25 14:23:31 +0300

    Добвлена фигура Треугольник

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
-﻿namespace СalculatingTheArea
+﻿using System;
+
+namespace СalculatingTheArea
 {
     class Program
     {
         static void Main(string[] args)
         {
+            double[]sides = { 1, 2, 3, 
+                              4, 0, 6, 
+                              6, -7, 8, 
+                              7, 3, 9}; 
+            double[] sidesTriangle = new double[3];
+
             IFlatFigure figure = new Circle();
 
             if (figure.IsCorrect)
@@ -25,6 +33,25 @@
             circle.SetSize(-5);
             if (circle.IsCorrect)
                 Console.WriteLine(@"The area of the figure - {0} = {1} ", circle.Name, circle.Area);
+
+
+            IFlatFigure figure2 = new Triangle();
+            if (figure2.IsCorrect)
+                Console.WriteLine(@"The area of the figure - {0} = {1} ", figure.Name, figure.Area);
+
+            for (int i = 0; i < sides.Length; i = i + 3)
+            {
+                Array.ConstrainedCopy(sides, i, sidesTriangle, 0, 3);
+                figure2.SetSize(sidesTriangle);
+                Console.WriteLine(@"Sides Triangle is {0} - {1}  - {2}", sidesTriangle[0], sidesTriangle[1], sidesTriangle[2]);
+                if (figure2.IsCorrect)
+                    Console.WriteLine(@"The area of the figure - {0} = {1} ", figure2.Name, figure2.Area);
+                Console.ReadLine();
+            } 
+                
+
+
+
 
         }
 
---
+++ Triangle.cs
@@ -0,0 +1,54 @@
+﻿
+namespace СalculatingTheArea
+{
+    internal class Triangle : IFlatFigure
+    {
+        private readonly string _name = "Triangle";
+        private List <double> _sides = new() { default, default, default };
+        private bool _isCorrect = false;
+
+        public string Name { get => _name; }
+
+        public bool IsCorrect { get => _isCorrect; }
+
+        public List<double> Sides { get => _sides; }
+
+        public double Area
+        {
+            get
+            {
+                if (IsCorrect)
+                {
+                    var h_perimeter = Sides.Take(3).Sum()/2;
+                    var square = h_perimeter;
+                    for (int i = 0; i < 3; i++)
+                        square = square*(h_perimeter - Sides[i]);
+                    return Math.Sqrt(square);
+                }
+                else return -1.0;
+            }
+        }
+
+        public Triangle(params double[] inputArray)
+        {
+            SetSize(inputArray);
+        }
+
+
+        public void SetSize(params double[] inputArray)
+        {
+            _isCorrect = !(inputArray == null || inputArray.Length < 3);
+
+            for  (int i = 0;  i < 3; i++)
+            {
+                _isCorrect = _isCorrect && inputArray[i] > 0;
+
+                if (_isCorrect) _sides[i] = inputArray[i];
+                
+                else _sides[i] = default;
+                
+            }
+            _isCorrect = IsCorrect && (Sides[0] < Sides[1]+Sides[2]) && (Sides[1] < Sides[0] + Sides[2]) && (Sides[2] < Sides[0] + Sides[1]);
+        }
+    }
+}
